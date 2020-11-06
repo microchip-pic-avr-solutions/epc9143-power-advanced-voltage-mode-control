4818178b540cf8fa804c3d47c1c2b0f5756fc5aa
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-11-06 15:47:55 +0100

    Updated doxygen generator source files. Correct output is generated when contents are generated by the batch file

--- .mchp_private/doxygen_src/build-output.bat
+++ .mchp_private/doxygen_src/build-output.bat
@@ -1,14 +1,25 @@
-@echo off 
-echo ##############  DOXYGEN OUTPUT GENERATION ##############
-rem  
-echo build doyxgen output...
-cd..\..\docs
-echo 
+@echo off
+echo ##############  DOXYGEN OUTPUT GENERATION  ##############
+echo 1) Clearing Output Directory:
+cd ..\..\docs
+echo Would you like to clear the output directory before generating new content?
 del *.*
-echo 
-cd..\.mchp_private\doxygen_src
-echo 
+echo Would you like to clear the image directory?
+del images\*.*
+done
+echo 2) Graphviz Package Version:
+rem To generate graphics like call graphs and flow charts, the Graphviz package needs to be installed.
+rem You can find this package here: https://www.graphviz.org/download/
+rem Please make sure the installation directory of Graphviz is registered as PATH variable
+rem Should doxygen still have trouble with calling the Dot tool, execute the following line 
+rem to make sure all library packages are properly registered 
+dot -V
+rem "C:\Program Files\Graphviz 2.44.1\bin\dot.exe" -c
+done
+echo 3) Calling Doxygen Generator:
+cd ..\.mchp_private\doxygen_src
 doxygen doxyfile
-echo 
-echo ##############  D O N E ##############
-TIMEOUT 15
+echo 4) Copying Image Folder:
+Xcopy /e /i /c /r /y ..\..\images ..\..\docs\images
+echo ##############  D O N E  ##############
+timeout 10
