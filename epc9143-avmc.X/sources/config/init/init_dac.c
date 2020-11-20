e962be8a944019e0b44460567d2020e6f41a644b
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-11-20 09:16:56 +0100

    Added DAC PRAL driver to project and replaced user configuration by library function calls

--- epc9143-avmc.X/sources/config/init/init_dac.c
+++ epc9143-avmc.X/sources/config/init/init_dac.c
@@ -6,25 +6,33 @@
  */
 
 
-#include <xc.h>
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include <stdint.h> // include standard integer types header file
+#include <stdbool.h> // include standard boolean types header file
+
 #include "config/hal.h"
 #include "init_dac.h"
+#include "pwr_control/devices/dev_buck_typedef.h"
 
 volatile uint16_t dac_mask = 0;
+volatile bool _dacout_used=false;
+
+/* Default configuration of DAC module running from 500 MHz input clock */
+volatile struct P33C_DAC_MODULE_s dacModuleDefault = {
+    .DacModuleCtrl1L.value = 0x0080, ///< DAC Module disabled, DAC Stop In Idle Mode=0, DAC Clock Source=AFPLLO, DAC Clock Divider=1:1, Filter Clock Divider=1:1
+    .DacModuleCtrl2H.bits.SSTIME = 0x008A, ///< Steady-state setting time is set to default of 0x8A
+    .DacModuleCtrl2L.bits.TMODTIME = 0x0055 ///< Transition mode duration is set to default of 0x55
+};
+
 
 volatile uint16_t init_dac_module(void) {
     
     volatile uint16_t retval=1;
+    volatile struct P33C_DAC_MODULE_s* dac;
 
-    DACCTRL1Lbits.DACON = 0; // Disable DAC module
-    
-    DACCTRL1Lbits.DACSIDL = 0; // Continues module operation in Idle mode
-    DACCTRL1Lbits.CLKSEL = 0b10; // Clock selection = AFPLLO
-    DACCTRL1Lbits.CLKDIV = 0b01; // Clock Divider selection = 1:2
-    DACCTRL1Lbits.FCLKDIV = 0b00; // Comparator Filter Clock Divider = 1:1
-
-    DACCTRL2Lbits.TMODTIME = 1; // set Transition Mode Duration
-    DACCTRL2Hbits.SSTIME = 1; // set Steady-State delay time
+    dac = p33c_DacModule_GetHandle();
+    retval &= p33c_DacModule_Dispose();
+    retval &= p33c_DacModule_ConfigWrite(dacModuleDefault);
 
     return (retval);
     
@@ -33,29 +41,19 @@ volatile uint16_t init_dac_module(void) {
 volatile uint16_t init_dac_channel(volatile uint16_t dacInstance) {
     
     volatile uint16_t retval=1;
-    volatile P33C_DAC_INSTANCE_t* dac;
+    volatile struct P33C_DAC_INSTANCE_s* dac;
     
-    dac = (volatile P33C_DAC_INSTANCE_t*)(
-                (volatile uint8_t*) &DAC1CONL + ((dacInstance - 1) * 
-                ((volatile uint16_t)&DAC2CONL - (volatile uint16_t)&DAC1CONL))
-        );
+    dac = p33c_DacInstance_GetHandle(dacInstance);
+    retval &= p33c_DacInstance_Dispose(dacInstance);
     
-    dac->DACxCONL.bits.DACEN = 0; // Disable DAC module #1
+    if(!_dacout_used)
+        dac->DACxCONL.bits.DACOEN = 1; // Enable DAC output pin
 
     dac->DACxCONH.bits.TMCB  = BUCK_LEB_PERIOD;
-    
-    dac->DACxCONL.bits.IRQM = 0b00; // disable generation of interrupts
-    dac->DACxCONL.bits.CBE = 0; // no comparator blanking bit 
-    dac->DACxCONL.bits.FLTREN = 0; // Disable digital filter
-    dac->DACxCONL.bits.CMPPOL = 0; // Comparator output not inverted
-    dac->DACxCONL.bits.INSEL = 0b000; // Enable Comparator input A
-    dac->DACxCONL.bits.HYSPOL = 0; // Hysteresis is applied to rising edge of comparator output
-    dac->DACxCONL.bits.HYSSEL = 0b00; // no hysteresis selected
-    dac->DACxCONL.bits.DACOEN = 1; // Disable DAC output pin
-    
-    dac->DACxDATH.value = 0x07FF;
-    
+    dac->DACxDATH.value = 0x07FF; // Set board current reference to VDD/2 (~1.650 V)
+
     dac_mask = (1 << (dacInstance-1));
+    _dacout_used |= (bool)(dac->DACxCONL.bits.DACOEN);
     
     return (retval);
 }
