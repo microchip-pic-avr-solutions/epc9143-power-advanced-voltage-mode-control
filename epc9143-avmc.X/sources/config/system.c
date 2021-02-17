14428dcdcc1ef67d2865ce2582e0eb84c4e36bd2
M91406 <andreas.reiter@microchip.com>
2021-02-17 10:46:38 +0100

    Bugfix: op-amp initialization was floored, accidentally overwriting previously configured op-amp instances when more than one op-amp was configured

--- epc9143-avmc.X/sources/config/system.c
+++ epc9143-avmc.X/sources/config/system.c
@@ -53,6 +53,9 @@ volatile uint16_t sysUserPeriperhals_Initialize(void) {
 
     volatile uint16_t retval=1;
     
+    // Reset the operation amplifier module to a disabled default state.
+    retval &= sysOpAmp_ModuleReset();
+    
     // Initialize op-amp
     retval &= sysOpAmp_Initialize(ISENSE_REF_BUFFER_OPA_INSTANCE, true); // Initialize op-amp #2 used to drive the reference voltage for current sense amplifiers
     
