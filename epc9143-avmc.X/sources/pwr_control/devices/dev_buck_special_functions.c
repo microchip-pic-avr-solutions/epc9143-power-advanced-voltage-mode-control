73d69264c1ea73efbd4df0c20ec7dabb18c9848e
M91406 <andreas.reiter@microchip.com>
2021-02-17 10:39:31 +0100

    Bugfix: current feedback calibration procedure averaged one step too short, producing a number which was slightly too low.

--- epc9143-avmc.X/sources/pwr_control/devices/dev_buck_special_functions.c
+++ epc9143-avmc.X/sources/pwr_control/devices/dev_buck_special_functions.c
@@ -211,7 +211,7 @@ volatile uint16_t CurrentSenseOffsetCalibration(volatile struct BUCK_POWER_CONTR
         }
 
         // Collect data samples
-        if (++calib_cs[_i].counter < CS_CALIB_STEPS)
+        if (calib_cs[_i].counter++ < CS_CALIB_STEPS)
         {
             calib_cs[_i].start = true; // Clear START flag bit
             calib_cs[_i].stop = false; // Clear STOP flag bit
