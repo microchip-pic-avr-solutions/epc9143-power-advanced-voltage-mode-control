2a8f1159defc7851d26da677a3cb681e1ce66372
M91406 <andreas.reiter@microchip.com>
2021-02-17 12:47:31 +0100

    Added setting of startup current limit reference to hardware description and incorporated setting in power supply configuration

--- epc9143-avmc.X/sources/config/hal/epc9143_r40_hwdescr.h
+++ epc9143-avmc.X/sources/config/hal/epc9143_r40_hwdescr.h
@@ -557,9 +557,13 @@
  * *************************************************************************************************/
 
 // Feedback Declarations
-#define BUCK_ISNS_FEEDBACK_GAIN     (float) 0.050       ///< Current Gain in V/A
-#define BUCK_ISNS_MINIMUM           (float) 0.000       ///< absolute minimum output current (average)
-#define BUCK_ISNS_MAXIMUM           (float) 26.50       ///< absolute maximum output current (average)
+#define BUCK_IOUT_MINIMUM           (float) 0.000       ///< absolute minimum output current (average)
+#define BUCK_IOUT_MAXIMUM           (float) 26.50       ///< absolute maximum output current (average)
+#define BUCK_IOUT_RELEASE           (float) 24.00       ///< current reset level after over current event
+
+#define BUCK_ISNS_FEEDBACK_GAIN     (float) 0.050       ///< Phase Current Gain in V/A
+#define BUCK_ISNS_MINIMUM           (float) 0.000       ///< absolute minimum phase current (average)
+#define BUCK_ISNS_MAXIMUM           (float) 26.50       ///< absolute maximum phase current (average)
 #define BUCK_ISNS_RELEASE           (float) 24.00       ///< current reset level after over current event
 #define BUCK_ISNS_REFERENCE         (float) 1.000       ///< output current reference (average)
 #define BUCK_ISNS_REFERENCE_STARTUP (float) 22.00       ///< maximum output current (average) at startup
@@ -567,8 +571,6 @@
 
 #define BUCK_ISNS1_FEEDBACK_OFFSET  (float) 1.650       ///< current sense #1 feedback offset (average)
 #define BUCK_ISNS2_FEEDBACK_OFFSET  (float) 1.650       ///< current sense #2 feedback offset (average)
-
-#define BUCK_ISNS_OFFSET_CALIBRATION_ENABLE false       ///< Current Sense Offset Calibration is disabled 
 
 /** @} */ // end of group
 
@@ -586,6 +588,10 @@
 // Phase Current Feedback Settings Conversion Macros
 #define BUCK_ISNS_REF           (uint16_t)((BUCK_ISNS_REFERENCE * BUCK_ISNS_FEEDBACK_GAIN) / ADC_GRANULARITY)  ///< Output Current Reference
 #define BUCK_ISNS_REF_STARTUP   (uint16_t)((BUCK_ISNS_REFERENCE_STARTUP * BUCK_ISNS_FEEDBACK_GAIN) / ADC_GRANULARITY)  ///< Output Current Startup Reference
+
+#define BUCK_IOUT_MIN           (uint16_t)(int16_t)((BUCK_IOUT_MINIMUM * BUCK_ISNS_FEEDBACK_GAIN) / ADC_GRANULARITY)  ///< Over Current Limit
+#define BUCK_IOUT_OCL           (uint16_t)((BUCK_IOUT_MAXIMUM * BUCK_ISNS_FEEDBACK_GAIN) / ADC_GRANULARITY)  ///< Over Current Limit
+#define BUCK_IOUT_OCL_RELEASE   (uint16_t)((BUCK_IOUT_RELEASE * BUCK_ISNS_FEEDBACK_GAIN) / ADC_GRANULARITY)  ///< Over Current Release Level
 
 #define BUCK_ISNS1_MIN          (uint16_t)(int16_t)(((BUCK_ISNS_MINIMUM-BUCK_ISNS1_FEEDBACK_OFFSET) * BUCK_ISNS_FEEDBACK_GAIN) / ADC_GRANULARITY)  ///< Over Current Limit
 #define BUCK_ISNS1_OCL          (uint16_t)(((BUCK_ISNS_MAXIMUM-BUCK_ISNS1_FEEDBACK_OFFSET) * BUCK_ISNS_FEEDBACK_GAIN) / ADC_GRANULARITY)  ///< Over Current Limit
