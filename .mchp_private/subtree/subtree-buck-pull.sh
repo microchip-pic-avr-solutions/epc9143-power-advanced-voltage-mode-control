0432b696131af62b9adbe70d0a994c8e814d64e9
M91406 <andreas.reiter@microchip.com>
2021-02-22 15:31:15 +0100

    Updated subtree repository management scripts

--- .mchp_private/subtree/subtree-buck-pull.sh
+++ .mchp_private/subtree/subtree-buck-pull.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 echo $SSH_AUTH_SOCK
 cd ../../
-git subtree pull --prefix="epc9143-avmc.X/sources/pwr_control/devices" subrepo-buck main --squash 
+echo Pulling changes from subtree 'subrepo-buck' branch 'main' into recently checked out working branch...
+git subtree pull --prefix="epc9143-avmc.X/sources/power_control/devices" subrepo-buck main --squash 
+echo Pull complete
+echo
 echo Press Enter to exit
 read
