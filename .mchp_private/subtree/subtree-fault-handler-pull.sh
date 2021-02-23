bad4d0471bd043875c2e4fb0f8898151be02cef4
Microchip Technology <microchip@microchip.com>
2021-02-23 08:40:47 +0000

    Merging into the master branch

--- .mchp_private/subtree/subtree-fault-handler-pull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo $SSH_AUTH_SOCK
-cd ../../
-git subtree pull --prefix="epc9143-avmc.X/sources/fault_handler/drivers" subrepo-fault main --squash
-echo Press Enter to exit
-read
-
--- .mchp_private/subtree/subtree-pral-pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo $SSH_AUTH_SOCK
-cd ../../
-git subtree pull --prefix="epc9143-avmc.X/sources/common/p33c_pral" subrepo-pral main --squash 
-echo Press Enter to exit
-read
